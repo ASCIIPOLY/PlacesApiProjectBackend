2c8b597b2f0adf0fb9ba3910585f3f4cf3fb2612
ASCIIPOLY <frknorn67@gmail.com>
2021-12-22 23:16:39 +0300

    Project created

---
+++ src/main/java/com/example/PlacesApiProject/PlacesApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlacesApiProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacesApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacesApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PlacesApiProject/PlacesApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlacesApiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacesApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
